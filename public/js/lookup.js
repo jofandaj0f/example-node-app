a77b8390ece29e171019931e1a10aa70b8f2456a
Jonathan Ferraro <jferraro@RNNTV.COM>
2019-04-02 21:25:12 -0400

    Second push for Downloading CSV

--- public/js/lookup.js
+++ public/js/lookup.js
@@ -5,55 +5,57 @@ function download_csv(data, name) {
   //   ['Moo', 'Reindeer Hunter']
   // ];
   var csv = 'Story Slug,Location,Reporter,Crew,WebEditorRequest,Writer,Path,GUID\n';
-  $.each( JSON.parse(data), function( key, value ) {
-      if(value.slug === 'ALL ZONES' || value.slug === 'LONG ISLAND' ||
-          value.slug === 'PHOTOGS LI' || value.slug === 'NEW JERSEY' ||
-        value.slug === 'PHOTOGS NJ' || value.slug === 'HUDSON VALLEY' ||
-      value.slug === 'PHOTOGS HV' || value.slug === 'FUTURES'){
-        continue;
+  $.each(JSON.parse(data), function( key, value ) {
+      if(value.slug == 'ALL ZONES' || value.slug == 'LONG ISLAND' ||
+          value.slug == 'PHOTOGS LI' || value.slug == 'NEW JERSEY' ||
+        value.slug == 'PHOTOGS NJ' || value.slug == 'HUDSON VALLEY' ||
+      value.slug == 'PHOTOGS HV' || value.slug == 'FUTURES' || value.slug == 'AJ TRUCK OP 930') {
+        console.log('Skipping header...');
       }
-      if (value.slug !== undefined){
-        csv += value.slug + ',';
+      else {
+        if (value.slug !== undefined){
+          csv += value.slug + ',';
+          // console.log(value.slug);
+        }
+        else if (value.slug === undefined){
+          csv += ',';
+        }
+        csv += value.location + ',';
+        if (value.reporter[0] !== undefined){
+          csv += value.reporter[0] + ',';
+        }
+        else if (value.reporter[0] === undefined){
+          csv += ',';
+        }
+        if (value.crew[0] !== undefined){
+          csv += value.crew[0] + ',';
+        }
+        else if (value.crew[0] === undefined){
+          csv += ',';
+        }
+        csv += value.webeditor + ',';
+        csv += value.writer + ',';
+        csv += value.path + ',';
+        csv += value.guid + ',';
+        csv += "\n"
       }
-      else if (value.slug === undefined){
-        csv += ',';
-      }
-      csv += value.location + ',';
-      if (value.reporter[0] !== undefined){
-        csv += value.reporter[0] + ',';
-      }
-      else if (value.reporter[0] === undefined){
-        csv += ',';
-      }
-      if (value.crew[0] !== undefined){
-        csv += value.crew[0] + ',';
-      }
-      else if (value.crew[0] === undefined){
-        csv += ',';
-      }
-      csv += value.webeditor + ',';
-      csv += value.writer + ',';
-      csv += value.path + ',';
-      csv += value.guid + ',';
-      csv += "\n"
-
   });
-
-  // console.log(csv);
+  console.log('Done Parsing Grid');
+  console.log('Downloading CSV file now...');
   var hiddenElement = document.createElement('a');
   hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
   hiddenElement.target = '_blank';
   hiddenElement.download = name;
   hiddenElement.click();
+  $('.lds-ellipsis').remove();
 }
 
-$(document).on('click', '#submit-ro, #submit-grid', function() {
+$(document).on('click', '#submit-grid', function() {
+  $('.col-md-3').append('<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>');
   var date = $('#theDate').val().toString();
   if (date.length !== 8) {
     alert('Select Date!');
   }
-  console.log(date);
-  console.log(date.length);
   $.ajax({
     url: '/api/v1/enps/writeGridtoFile',
     type: 'POST',
@@ -64,6 +66,25 @@ $(document).on('click', '#submit-ro, #submit-grid', function() {
     })
   }).done(function(data) {
     var fileName = "Fios1News_Planning_Grid_20" + $('#theDate').val() + ".csv";
+    download_csv(data, fileName);
+  });
+});
+
+$(document).on('click', '#submit-ro', function() {
+  var date = $('#theDate').val().toString();
+  if (date.length !== 8) {
+    alert('Select Date!');
+  }
+  $.ajax({
+    url: '/api/v1/enps/writeROtoFile',
+    type: 'POST',
+    datatype: 'json',
+    contentType: 'application/json',
+    data: JSON.stringify({
+    	"date" : date
+    })
+  }).done(function(data) {
+    var fileName = "Fios1News_Rundown_20" + $('#theDate').val() + ".csv";
     download_csv(data, fileName);
   });
 });
