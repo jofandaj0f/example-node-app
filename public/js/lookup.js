d5232770fd037814635dc0956fc1ba5c983cb0c1
Jonathan Ferraro <jferraro@RNNTV.COM>
2019-04-08 20:43:36 -0400

    Extra change

--- public/js/lookup.js
+++ public/js/lookup.js
@@ -4,17 +4,71 @@ function download_csv(data, name, type) {
   var zone = "";
   if (type == 'grid') {
     csv += "Story Slug,Event Time,Location,Zone,Type,Status,Reporter,Crew,V R X,Path,GUID\n";
-    $.each(data, function(key, value) {
+    $.each(JSON.parse(data), function(key, value) {
 
-      if ( value.slug == "PHOTOGS LI" || value.slug == "PHOTOGS NJ" ||
-       value.slug == "PHOTOGS HV"  || value.slug == "AJ TRUCK OP 930" || value.slug == "FUTURES") {
+      if (value.slug == "PHOTOGS LI" || value.slug == "PHOTOGS NJ" ||
+        value.slug == "PHOTOGS HV" || value.slug == "AJ TRUCK OP 930" || value.slug == "FUTURES") {
         console.log("Skipping Header...");
       }
       if (value.slug == "ALL ZONES" || value.slug == "LONG ISLAND" ||
-       value.slug == "NEW JERSEY" || value.slug == "HUDSON VALLEY" ) {
+        value.slug == "NEW JERSEY" || value.slug == "HUDSON VALLEY") {
         zone = value.slug;
-      } if (value.reporter[0] === undefined && value.crew[0] === undefined) {
+      }
+      if (value.reporter[0] === undefined && value.crew[0] === undefined) {
         console.log("No assigned field staff...");
+      }
+      if (value.reporter[0] !== undefined && value.crew[0] !== undefined) {
+        if (value.slug !== undefined) {
+          var vrx = "";
+          var slugger = value.slug.replace(/,/g, '');
+          csv += slugger + ",";
+          // console.log(value.slug);
+        } else if (value.slug === undefined) {
+          csv += ",";
+        }
+        csv += value.eventTime + ",";
+        csv += value.location + ",";
+        csv += zone + ",";
+        csv += value.type + ",";
+        csv += value.status + ",";
+        if (value.reporter[0] !== undefined) {
+          var lengthOff = value.reporter[0].length;
+          csv += value.reporter[0].substring(17, lengthOff) + ",";
+          vrx += "R";
+        } else if (value.reporter[0] === undefined) {
+          csv += ",";
+        }
+        csv += ",";
+        csv += vrx + ",";
+        csv += value.path + ",";
+        csv += value.guid + ",";
+        csv += "\n";
+        if (value.slug !== undefined) {
+          var vrx = "";
+          var slugger = value.slug.replace(/,/g, '');
+          csv += slugger + ",";
+          // console.log(value.slug);
+        } else if (value.slug === undefined) {
+          csv += ",";
+        }
+        csv += value.eventTime + ",";
+        csv += value.location + ",";
+        csv += zone + ",";
+        csv += value.type + ",";
+        csv += value.status + ",";
+        csv += ",";
+        if (value.crew[0] !== undefined) {
+          var lengthOf = value.crew[0].length;
+          csv += value.crew[0].substring(6, lengthOf) + ",";
+          vrx += "V";
+        } else if (value.crew[0] === undefined) {
+          csv += ",";
+        }
+        csv += vrx + ",";
+        csv += value.path + ",";
+        csv += value.guid + ",";
+        csv += "\n";
+        // General fields
       } else {
         if (value.slug !== undefined) {
           var vrx = "";
@@ -46,7 +100,7 @@ function download_csv(data, name, type) {
         csv += vrx + ",";
         csv += value.path + ",";
         csv += value.guid + ",";
-        csv += "\n"
+        csv += "\n";
       }
     });
   }
@@ -58,7 +112,6 @@ function download_csv(data, name, type) {
         var dash = value.Title.indexOf('-') + 1;
         var slugLen = value.Title.length;
         var slugger = value.Title.replace(/,/g, '');
-
         csv += slugger.substring(0, value.Title.indexOf('-')) + ",";
         csv += value.Title.substring(dash, slugLen) + ",";
         // console.log(value.slug);
@@ -66,12 +119,11 @@ function download_csv(data, name, type) {
         csv += ",";
         csv += ",";
       }
-      console.log(value.WRNNXpressionMOS);
-      if (value.WRNNXpressionMOS !== undefined){
+      if (value.WRNNXpressionMOS !== undefined) {
         var len;
         var banners = "";
-        for (var i = 0; value.WRNNXpressionMOS.length; i++){
-          if (value.WRNNXpressionMOS[i] === undefined){
+        for (var i = 0; value.WRNNXpressionMOS.length; i++) {
+          if (value.WRNNXpressionMOS[i] === undefined) {
             break;
           }
           console.log(value.WRNNXpressionMOS[i], i, len);
@@ -79,13 +131,12 @@ function download_csv(data, name, type) {
           banners += value.WRNNXpressionMOS[i].substring(bannerDel, value.WRNNXpressionMOS[i].length).replace(/,/g, "") + " ";
         }
         csv += banners + ",";
-      } else if (value.WRNNXpressionMOS === undefined){
+      } else if (value.WRNNXpressionMOS === undefined) {
         csv += ",";
       }
-      if(value.WRNNCrispNew[0] !== undefined){
-          csv += value.WRNNCrispNew[0] + ",";
-      }
-      else {
+      if (value.WRNNCrispNew[0] !== undefined) {
+        csv += value.WRNNCrispNew[0] + ",";
+      } else {
         csv += ",";
       }
       // if(value.MOSItemDuration !== undefined){
@@ -96,16 +147,14 @@ function download_csv(data, name, type) {
       // }
       csv += value.FrontTime + ",";
       csv += value.BackTime + ",";
-      if(value.RowModBy !== undefined){
-          csv += value.RowModBy + ",";
-      }
-      else {
+      if (value.RowModBy !== undefined) {
+        csv += value.RowModBy + ",";
+      } else {
         csv += ",";
       }
-      if(value.Estimated !== undefined){
-          csv += value.Estimated + ",";
-      }
-      else {
+      if (value.Estimated !== undefined) {
+        csv += value.Estimated + ",";
+      } else {
         csv += ",";
       }
       csv += value.Float + ",";
@@ -132,8 +181,7 @@ $(document).on('click', '#listRundowns', function() {
   var enpsFolder = $('#zones').val().toString();
   if (date.length !== 8) {
     alert('Select Date!');
-  }
-  else {
+  } else {
     $('#loader').append('<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>');
     $.ajax({
       url: '/api/v1/enps/listRundowns',
@@ -166,8 +214,7 @@ $(document).on('click', '#submit-grid', function() {
   var date = $('#theDate').val().toString();
   if (date.length !== 8) {
     alert('Select Date!');
-  }
-  else {
+  } else {
     $('#loader').append('<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>');
     $.ajax({
       url: '/api/v1/enps/writeGridtoFile',
@@ -188,8 +235,7 @@ $(document).on('click', '#download-ro', function() {
   var date = $('#theDate').val().toString();
   if (date.length !== 8) {
     alert('Select Date!');
-  }
-  else {
+  } else {
     $('#loader').append('<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>');
     $.ajax({
       url: '/api/v1/enps/writeROtoFile',
